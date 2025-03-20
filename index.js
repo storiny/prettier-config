36301a74a227f8b6dba3f639f3713cc3f3d4ba88
zignis <velociraptorm77@gmail.com>
2025-03-20 16:13:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+    endOfLine: "auto",
+    printWidth: 80,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 2,
+    trailingComma: "none",
+};
+
+export default config;
